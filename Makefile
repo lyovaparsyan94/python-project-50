26c5df5e5d85470a22ac0256c0ba354fae7db91e
Parsyan Lyova <l.parsyan@blask.com>
2025-02-15 00:31:29 +0400

    feat: implement diff generation and formatting updates
    - Add GitHub Actions workflow configuration
    - Enhance diff generation scripts in gendiff module
    - Improve parser functionality
    - Update expected test results for JSON and YAML formats

--- Makefile
+++ Makefile
@@ -6,3 +6,9 @@ build:
 	uv build
 package-install:
 	uv tool install dist/*.whl
+make lint:
+	uv run ruff check gendiff
+make tests:
+	uv run pytest
+make test-coverage:
+	uv run pytest --cov
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,7 @@
 import argparse
 
+from gendiff.scripts.generate_diff import generate_diff
+
 
 def parser_function():
     parser = argparse.ArgumentParser(
@@ -7,16 +9,22 @@ def parser_function():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument("-f", "--format", help='set format of output')
+    parser.add_argument("-f", "--format",
+                        help='set format of output',
+                        default='stylish', type=str
+    )
 
-
-    args = parser.parse_args()
-    print(args)
-
+    return parser.parse_args()
 
 
 def main():
-    parser_function()
+    args = parser_function()
+
+    file_path1 = args.first_file
+    file_path2 = args.second_file
+
+    result = generate_diff(file_path1, file_path2, formatter=args.format)
+    print(result)
 
 
 if __name__ == "__main__":
