3c6c1ecfdd80f0caf921aa80ea67a69a6bb26962
Parsyan Lyova <l.parsyan@blask.com>
2025-05-20 13:13:17 +0400

    replace repository

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,17 +1,5 @@
-import gendiff.formatters.json_formatter as json_formatter
-import gendiff.formatters.stylish_formatter as stylish_formatter
-import gendiff.modules.gendiff as gendiff
-from gendiff.formatters.json_formatter import format_json
-from gendiff.formatters.plain_formatter import format_plain
-from gendiff.formatters.stylish_formatter import format_stylish
-from gendiff.modules.gendiff import generate_diff
+from gendiff.scripts.generate_diff import generate_diff
 
 __all__ = (
-    "gendiff",
     "generate_diff",
-    "stylish_formatter",
-    "format_stylish",
-    "format_plain",
-    "format_json"
-
 )
